bcd61a38290974eae759f3a2db3eb46beca68271
Shahil Rauf <Shahilzr@gmail.com>
2024-08-22 20:39:41 -0700

    Create 6. Average number of different expiration dates per SKU in-stock, by storage type.py

---
+++ 6. Average number of different expiration dates per SKU in-stock, by storage type.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Load the data
+transaction_log = pd.read_csv('wms_inventory_transaction_log.csv')
+
+# Count distinct expiration dates per SKU
+expiration_dates_count = inventory_summary.groupby('item_number')['expiration_timestamp'].nunique().reset_index()
+
+# Merge with item info to get storage type
+expiration_dates_count = pd.merge(expiration_dates_count, item_info, on='item_number')
+
+# Calculate average number of expiration dates per storage type
+avg_expiration_dates = expiration_dates_count.groupby('storage_type')['expiration_timestamp'].mean()
+print(avg_expiration_dates)
